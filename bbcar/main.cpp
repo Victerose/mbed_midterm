4ec3292bdd809e965a7fa3a9a2acdb236824f68c
王項 <victerose@yahoo.com.tw>
2018-04-28 18:55:58 +0800

    finish bbcar

--- bbcar/main.cpp
+++ bbcar/main.cpp
@@ -16,7 +16,7 @@ parallax_ping ping_high(pin3); // high ping
 PwmOut pin13(D13); // pin13 : left conti. servo
 PwmOut pin11(D11); // pin11 : right conti. servo
 
-// catcher
+// gun
 PwmOut pin12(D12);
 
 // decide whether see the ball
@@ -34,35 +34,56 @@ int main() {
 
     servo_ticker.attach(&servo_control, .5);
 
-    parallax_servo catcher(pin12);
+    parallax_servo gun(pin12);
+    gun = 500;
 
     parallax_servo left_servo(pin13);
+    left_servo = 0;
     parallax_servo right_servo(pin11);
+    right_servo = 0;
     left_servo.set_ramp(50);
     right_servo.set_ramp(50);
 
-    DigitalOut ping7(D7);
+    // shotter
+    DigitalOut shotter1(D7);
+    shotter1 = 1;
+    DigitalOut shotter2(D6);
+    shotter2 = 1;
     //printf("Ping = %lf\r\n", (float)ping_low); // see the distance
 
+    printf("GOGO");
+    wait(1);
+    
     while(1) {
-        pc.printf("Ping = %lf\r\n", (float)ping_low);
         
+        printf("Ping = %lf\r\n", (float)ping_low);
+        wait(0.2);
+
         if(ping_low < detect_area){ // see the target
-            left_servo = -2; right_servo = 2;
-            wait(0.1);
+            left_servo = 0; right_servo = 0;
+            wait(1);
+            left_servo = -10; right_servo = -10;
+            wait(0.5);
+            shotter1 = 0; shotter2 = 0;
+            wait(1);
+            gun = 0;
+            wait(1);
+            printf("finish shotting");
+            shotter1 = 1; shotter2 = 1;
+            wait(1);
+            gun = 500;
+            break;
         }
         else{
             left_servo = 20; right_servo = 20;
-            wait(0.1);
         }
-
-       // shoter
-       ping7 = 0;
-       wait(1);
-       ping7 = 1;
-       wait(1);
-       
+        printf("looping");
+        wait(0.2);
     }
+
+    printf("finish all");
+    wait(1);
+    return 0;
 
 }
 
